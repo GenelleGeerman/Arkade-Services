e7cc1849777586045c8a445d7370d860a9f4c506
Genelle Geerman <g.geerman@student.fontys.nl>
2024-06-23 16:02:47 +0200

    Update ReviewContext.cs

--- ReviewService/RepositoryLayer/ReviewContext.cs
+++ ReviewService/RepositoryLayer/ReviewContext.cs
@@ -8,4 +8,10 @@ public class ReviewContext : DbContext
     public ReviewContext(DbContextOptions<ReviewContext> options) : base(options) { }
 
     public DbSet<ReviewEntity> Reviews { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ReviewEntity>()
+            .Property(e => e.RowVersion)
+            .IsRowVersion();
+    }
 }
